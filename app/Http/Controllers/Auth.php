f145b0933ed53c0b64fbbef3d812b6d2c3f2c305
Renz Castaloni <castalonirenz@gmail.com>
2019-09-18 23:26:55 +0800

    logs show in auth

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Login;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 class Auth extends Controller
 {
@@ -17,12 +18,15 @@ class Auth extends Controller
       $user = Login::where(['cust_username' => $request->input('username'), 
         'cust_password' => $request->input('password')])->first();
 
-       
+      
         if($user !== null){
-            return
-            response()
+            $logs = DB::table('log')
+                        ->where(['cust_id' => $user['cust_id']])
+                        ->get();
+            return response()
             ->json(['status' => 'success', 
-                'data'=>$user]);
+                'data'=>$user,
+                'logs'=>$logs]);
             
             
         }
